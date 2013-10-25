9c1d2d01d91bbc96ed5f04057ee0ffcfd6fb2685
Leo Bärring <leo.barring@gmail.com>
2013-10-25 03:30:22 +0200

    font load/draw routines
    specifically pcf v2 and ignoring any unicode table at the end
    
    Signed-off-by: Leo Bärring <leo.barring@gmail.com>

---
+++ src/font.c
@@ -0,0 +1,72 @@
+#include "font.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <string.h>
+
+
+pcf2_font_t *font_load(const char *fn) { /*{{{*/
+	FILE *fd = fopen(fn, "rb");
+	assert(fd);
+	pcf2_font_t *f = malloc(sizeof(pcf2_font_t));
+	assert(f);
+	fread(&f->header, sizeof(pcf2_header_t), 1, fd);
+	f->bitmap = malloc(sizeof(uint8_t)*
+		f->header.charsize*f->header.length
+	);
+	fread(f->bitmap, sizeof(uint8_t), f->header.charsize*f->header.length, fd);
+	fclose(fd);
+	f->bpr = f->header.charsize / f->header.height;
+} /*}}}*/
+void font_free(pcf2_font_t *font) { /*{{{*/
+	free(font->bitmap);
+	free(font);
+} /*}}}*/
+void draw_glyph(bitmap_t *canvas,  int x,  int y, unsigned int c, const pcf2_font_t *f, const u83_t *fg) { /*{{{*/
+	for (int dy=0; dy<f->header.height; ++dy) {
+		for (int dbx=0; dbx<f->bpr; ++dbx) {
+			uint8_t fb = f->bitmap[c*f->header.charsize + dy*f->bpr + dbx];
+			for (int dx=0; dx<8 && dx+dbx*8<f->header.width; ++dx) {
+				int canvasx = x + dbx*8 + dx;
+				int canvasy = y + dy;
+				if (canvasx>=0 && canvasy>=0 && canvasx<canvas->width && canvasy<canvas->height) {
+					unsigned int canvasi = canvasy*canvas->width + canvasx;
+					if ((fb & (1<<(7-dx))) != 0) {
+						memcpy(canvas->data+canvasi, fg, sizeof(u83_t));
+					}
+				}
+			}
+		}
+	}
+} /*}}}*/
+void draw_string(bitmap_t *canvas,  int x,  int y, const char *s, const pcf2_font_t *f, const u83_t *fg){ /*{{{*/
+	for (unsigned int i=0; i<strlen(s); ++i) {
+		draw_glyph(canvas, x+i*f->header.width, y, (uint8_t)s[i], f, fg);
+	}
+} /*}}}*/
+void draw_glyph_sc(bitmap_t *canvas, int x, int y, unsigned int c, const pcf2_font_t *f, const u83_t *fg, unsigned int sc) { /*{{{*/
+	for (int dy=0; dy<f->header.height; ++dy) {
+		for (int dbx=0; dbx<f->bpr; ++dbx) {
+			uint8_t fb = f->bitmap[c*f->header.charsize + dy*f->bpr + dbx];
+			for (int dx=0; dx<8 && dx+dbx*8<f->header.width; ++dx) {
+				for (int sy=0; sy<1<<sc; ++sy) {
+					for (int sx=0; sx<1<<sc; ++sx) {
+						int canvasx = sx+x + ((dbx*8 + dx)<<sc);
+						int canvasy = sy+y + (dy<<sc);
+						if (canvasx>=0 && canvasy>=0 && canvasx<canvas->width && canvasy<canvas->height) {
+							unsigned int canvasi = canvasy*canvas->width + canvasx;
+							if ((fb & (1<<(7-dx))) != 0) {
+								memcpy(canvas->data+canvasi, fg, sizeof(u83_t));
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+} /*}}}*/
+void draw_string_sc(bitmap_t *canvas, int x, int y, const char *s, const pcf2_font_t *f, const u83_t *fg, unsigned int sc) { /*{{{*/
+	for (unsigned int i=0; i<strlen(s); ++i) {
+		draw_glyph_sc(canvas, x+i*f->header.width*(1<<sc), y, (uint8_t)s[i], f, fg, sc);
+	}
+} /*}}}*/
---
+++ src/font.h
@@ -0,0 +1,31 @@
+#ifndef __FONT_H__
+#define __FONT_H__
+#include "graphics.h"
+#include <stdint.h>
+
+// No unicode stuff supported yet.
+
+typedef struct {
+    uint8_t  magic[4];
+    uint32_t version;
+    uint32_t headersize;
+    uint32_t flags;
+    uint32_t length;
+    uint32_t charsize;
+    uint32_t height;
+    uint32_t width;
+} pcf2_header_t;
+
+typedef struct {
+	pcf2_header_t header;
+	unsigned int bpr;
+	uint8_t *bitmap;
+} pcf2_font_t;
+
+pcf2_font_t *font_load(const char* fn);
+void font_free(pcf2_font_t *font);
+void draw_glyph(bitmap_t *canvas, int x, int y, unsigned int c, const pcf2_font_t *f, const u83_t *fg);
+void draw_string(bitmap_t *canvas, int x, int y, const char *s, const pcf2_font_t *f, const u83_t *fg);
+void draw_glyph_sc(bitmap_t *canvas, int x, int y, unsigned int c, const pcf2_font_t *f, const u83_t *fg, unsigned int sc);
+void draw_string_sc(bitmap_t *canvas, int x, int y, const char *s, const pcf2_font_t *f, const u83_t *fg, unsigned int sc);
+#endif
